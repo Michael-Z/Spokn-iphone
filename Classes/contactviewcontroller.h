93d1804f0604b9f537d4d32d4d773623b27f7ebb
Rishi Saxena <kaustubh@Rishi-Mac-Mini.local>
2010-01-13 18:19:33 +0530

    change in all files for licencing

--- Classes/contactviewcontroller.h
+++ Classes/contactviewcontroller.h
@@ -5,6 +5,24 @@
 //  Created by Mukesh Sharma on 05/07/09.
 //  Copyright 2009 Geodesic Ltd.. All rights reserved.
 //
+/**
+ Copyright 2009 John Smith, <john.smith@example.com>
+ 
+ This file is part of FOOBAR.
+ 
+ FOOBAR is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ FOOBAR is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with FOOBAR.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 #import "Ltptimer.h"
