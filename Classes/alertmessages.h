e384f30d5796a60143373463c69d4d1098617e1d
Rishi Saxena <kaustubh@Rishi-Mac-Mini.local>
2010-01-18 17:36:12 +0530

    alertmassages.h header added

---
+++ Classes/alertmessages.h
@@ -0,0 +1,50 @@
+/*
+ *  alertmessages.h
+ *  spokn
+ *
+ *  Created by Rishi Saxena on 18/01/10.
+ *  Copyright 2010 Geodesic Ltd. All rights reserved.
+ *
+ */
+
+
+#ifndef _ALERT_MESSAGES_H_
+	#define _ALERT_MESSAGES_H_
+
+
+#define _EMPTY_CONTACT_DETAILS_ @"This contact does not have any Number or Email address."
+#define _EMPTY_USERNAME_ @"User name cannot be empty"
+#define _EMPTY_PASSWORD_ @"Password cannot be empty"
+#define _USERNAME_NO_WHITESPACE_ @"User name should not contain white space"
+#define _PASSWORD_NO_WHITESPACE_ @"Password should not contain white space"
+#define _INVALID_NUMBER_ @"Please enter a valid number."
+#define _NOT_ONLINE_ @"User not online"
+#define _NO_NETWORK_ @"No Network"
+#define _ENTER_NUMEBR_ @"Please enter the number."
+#define _SERVER_UNREACHABLE_ @"Server not reachable"
+#define _AUTHENTICATION_FAILED_ @"Authentication failed"
+#define _NO_AVAILABLE_NETWORK_ @"Network is not available"
+#define _CONNECTION_FAILED_ @"Unable to connect server."
+#define _EMPTY_FORWARD_NUMBER_ @"Forward number should not be empty"
+#define _VMS_ALREADY_PLAYING @"Vms is already playing"
+#define _VMS_NOT_FULLY_DOWNLOADED @"This can be due to slow Internet access.\r\nDo you want to try downloading now?"
+#define _CLEAR_VMS_LOG_ @"Are u sure you want to clear vmail?"
+#define _INVALID_EMAIL_ @"Invalid email address"
+#define _CONTACT_DELETE_ @"Are you sure you want to delete contact?"
+#define _INVALID_CONTACT_ @"Invalid contact"
+#define _DELETE_CALL_LOG_ @"Are you sure you want to delete calllog?"
+#define _VMS_SENT_ @"VMS sent successfuly."
+#define _VMS_SENDING_FAILED_ @"VMS sending failed."
+#define _EMPTY_NUMBER_ @"Number should not be empty"
+#define _DELETE_VMS_ @"Are you sure you want to delete this voice mail?"
+#define _NO_VMS_PLAY_ @" can not play vms"
+#define _CLEAR_CALL_LOG_ @"Clear all calls?"
+#define _DUPLICATE_FORWARD_NUMBER_ @"The number you provided for call forwarding has already been used by someone else.Please provide another number and try again."
+#define _NO_CREDITS_ @"Your VMS could not be sent because you do not have enough credit. Please recharge & try again."
+#define _NO_WIFI_ @"Wi-Fi not available"
+#define _NO_MICROPHONE_ @"No Microphone Available"
+#define _USER_OFFLINE_ @"User not online"
+
+
+
+#endif
