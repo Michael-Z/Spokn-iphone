cd4881469092097bd036ac7a5a874bad2125bff5
Rishi Saxena <kaustubh@Rishi-Mac-Mini.local>
2010-02-10 19:16:39 +0530

    change in project for ui

--- Classes/alertmessages.h
+++ Classes/alertmessages.h
@@ -11,6 +11,8 @@
 #ifndef _ALERT_MESSAGES_H_
 	#define _ALERT_MESSAGES_H_
 
+#define _TEXTCOLOR_ 42/255.0 green:116/255.0 blue:217/255.0 alpha:1.0
+#define _REDCOLOR_ 187/255.0 green:25/255.0 blue:25/255.0 alpha:1.0
 
 #define _EMPTY_CONTACT_DETAILS_ @"This contact does not have any Number or Email address."
 #define _EMPTY_USERNAME_ @"User name cannot be empty"
@@ -39,7 +41,7 @@
 #define _VMS_RECORDING_MSG2_ @"Press done to finish recording."
 #define _VMS_RECORDING_MSG3_ @"You may send, preview or re-record the VMS now"
 #define _EMPTY_NUMBER_ @"Number field should not be empty."
-#define _DELETE_VMS_ @"Are you sure you want to delete this voice mail?"
+//#define _DELETE_VMS_ @"Are you sure you want to delete this voice mail?"
 #define _NO_VMS_PLAY_ @" can not play vms"
 #define _CLEAR_CALL_LOG_ @"Clear all calls"
 #define _DUPLICATE_FORWARD_NUMBER_ @"The number you provided for call forwarding has already been used by someone else.Please provide another number and try again."
